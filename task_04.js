0dc0b70c35b93338e8d64cc84d5781e8c4f54d16
Dima Gusonka <gusonkad@gmail.com>
2021-03-27 23:13:19 +0200

    Add fourth task to js_hw_04

--- task_04.js
+++ task_04.js
@@ -1,1 +1,19 @@
 // task_04.js
+const countTotalSalary = function(employees) {
+   let sumSalary=0;
+   for (const employee in employees) {
+      sumSalary+=employees[employee];
+   }
+   return sumSalary;
+}
+console.log(countTotalSalary({})); // 0
+console.log(countTotalSalary({
+    mango: 100,
+    poly: 150,
+    alfred: 80,
+  }),); // 330
+console.log(countTotalSalary({
+    kiwi: 200,
+    lux: 50,
+    chelsy: 150,
+  }),); // 400

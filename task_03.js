b9ca33e1281ab44b22c08780a6b0c7d6893da6db
Dima Gusonka <gusonkad@gmail.com>
2021-03-27 23:01:46 +0200

    Add third task to js_hw_03

--- task_03.js
+++ task_03.js
@@ -1,1 +1,30 @@
 // task_03.js
+const findBestEmployee = function(employees) {
+   let max = 0;
+   let bestEmployee;
+   for (const employee in employees) {
+       const tasksSum = employees[employee];
+       if (tasksSum > max) {
+           max = tasksSum;
+           bestEmployee = employee;
+       }
+   }
+   return bestEmployee;
+}
+console.log(findBestEmployee({
+     ann: 29,
+     david: 35,
+     helen: 1,
+     lorence: 99,
+   }),); // lorence
+ console.log(findBestEmployee({
+     poly: 12,
+     mango: 17,
+     ajax: 4,
+   }),); // mango
+ console.log(findBestEmployee({
+     lux: 147,
+     david: 21,
+     kiwi: 19,
+     chelsy: 38,
+   }),); // lux

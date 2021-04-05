296faf788f1bae54991d60763df87e9f2314823b
Gusonkad <gusonkad@gmail.com>
2021-04-05 17:36:55 +0300

    Add obj to add_task_10

---
+++ add_tasks_hw_03/add_task_10.js
@@ -0,0 +1,28 @@
+// add_task_10.js to home work 03
+
+const Sizes = {
+   BIG: { price: 25, cal: 100, time: 15 },
+   SMALL: { price: 15, cal: 50, time: 7 },
+   MEDIUM: { price: 15, cal: 50, time: 7 },
+}
+
+const Stuffing = {
+   CHEESE: { price: 6.5, cal: 45, time: 2 },
+   BEACON: { price: 10, cal: 70, time: 6 },
+   TOMATO: { price: 12.1, cal: 4, time: 5 },
+   CHICKEN: { price: 9.3, cal: 30, time: 5.1 },
+
+}
+
+const Sauces = {
+   MUSTARD: { price: 3, cal: 5, time: 1 },
+   KETCHUP: { price: 4.2, cal: 20, time: 1.5 },
+   BOLOGNESE: { price: 7.5, cal: 50, time: 3 },
+}
+
+
+
+addItem.newItem(Stuffing, 'MUSHROOMS', 'price', 9.3, 'cal', 30, 'time', 5.1)
+console.log(Stuffing);
+
+
